c196e80b6ecc1a3a6c21331dcb40665cc9c1debb
Gabriel Mayorga <gabrielmayorgate@gmail.com>
2023-02-22 03:57:42 -0600

    Full funcionality finished

--- main.js
+++ main.js
@@ -3,26 +3,32 @@ const desktopMenu = document.querySelector('.desktop-menu')
 const burgerMenuIcon = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 const shoppingCartIcon = document.querySelector('.navbar-shopping-cart')
-const aside = document.querySelector('.product-detail')
+const shoppingCart = document.querySelector('#shopping-cart')
 const cardsContainer = document.querySelector('.cards-container')
+const productDetailContainer = document.querySelector('#product-detail')
+const productDetailCloseIcon = document.querySelector('.product-detail-close')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 burgerMenuIcon.addEventListener('click', toggleMobileMenu)
 shoppingCartIcon.addEventListener('click', toggleCartAside)
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside)
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive')
-    aside.classList.add('inactive')
+    shoppingCart.classList.add('inactive')
+    productDetailContainer.classList.add('inactive')
 }
 function toggleMobileMenu(){
     mobileMenu.classList.toggle('inactive')
-    aside.classList.add('inactive')
+    shoppingCart.classList.add('inactive')
+    productDetailContainer.classList.add('inactive')
 }
 
 function toggleCartAside(){
-    aside.classList.toggle('inactive')
+    shoppingCart.classList.toggle('inactive')
     mobileMenu.classList.add('inactive')
     desktopMenu.classList.add('inactive')
+    productDetailContainer.classList.add('inactive')
 }
 
 const productList = []
@@ -33,42 +39,55 @@ productList.push({
     image: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/09c5ea6df1bd4be6baaaac5e003e7047_9366/Forum_Low_Shoes_White_FY7756_01_standard.jpg'
 })
 
+function openProductDetailAside(){
+    productDetailContainer.classList.remove('inactive')
+    shoppingCart.classList.add('inactive')
+}
+
+function closeProductDetailAside(){
+    productDetailContainer.classList.add('inactive')
+}
+
 function renderProducts(arr){
     let product;
+    let i = 0
+    do {
+        for (product of arr){
+            const productCard = document.createElement('div')
+            productCard.classList.add('product-card')
 
-    for (product of arr){
-        const productCard = document.createElement('div')
-        productCard.classList.add('product-card')
+            const productImg = document.createElement('img')
+            productImg.setAttribute('src', product.image)
+            productImg.classList.add('product-img')
+            productImg.addEventListener('click', openProductDetailAside)
 
-        const productImg = document.createElement('img')
-        productImg.setAttribute('src', product.image)
-        productImg.classList.add('product-img')
+            const productInfo = document.createElement('div')
+            productInfo.classList.add('product-info')
 
-        const productInfo = document.createElement('div')
-        productInfo.classList.add('product-info')
+            const productInfoDiv = document.createElement('div')
 
-        const productInfoDiv = document.createElement('div')
+            const productPrice = document.createElement('p')
+            productPrice.innerText = '$' + product.price
+            const productName = document.createElement('p')
+            productName.innerText = product.name
 
-        const productPrice = document.createElement('p')
-        productPrice.innerText = '$' + product.price
-        const productName = document.createElement('p')
-        productName.innerText = product.name
+            productInfoDiv.append(productPrice, productName)
 
-        productInfoDiv.append(productPrice, productName)
+            const productInfoFigure = document.createElement('figure')
+            const productInfoCart = document.createElement('img')
+            productInfoCart.setAttribute('src', './icons/add-cart.png')
 
-        const productInfoFigure = document.createElement('figure')
-        const productInfoCart = document.createElement('img')
-        productInfoCart.setAttribute('src', './icons/add-cart.png')
+            productInfoFigure.appendChild(productInfoCart)
 
-        productInfoFigure.appendChild(productInfoCart)
+            productInfo.appendChild(productInfoDiv)
+            productInfo.appendChild(productInfoFigure)
 
-        productInfo.appendChild(productInfoDiv)
-        productInfo.appendChild(productInfoFigure)
+            productCard.append(productImg, productInfo)
 
-        productCard.append(productImg, productInfo)
-
-        cardsContainer.appendChild(productCard)
-    }
+            cardsContainer.appendChild(productCard)
+        }
+        i++
+    }while(i < 14)
 }
 
 renderProducts(productList)
